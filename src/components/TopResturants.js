24b47667b55e7a09785a2bb3da65a31fc95ab53a
Shaikhmohddanish <danishshaikh2365@gmail.com>
2025-09-22 20:43:18 +0530

    Refactor TopRestaurants component to implement search functionality

--- src/components/TopResturants.js
+++ src/components/TopResturants.js
@@ -1,17 +1,27 @@
 import RestaurantCard from "./ResturantCard";
 import {TOP_RESTAURANTS_NEAR_ME} from "../constant";
 import "./search.css"
+import { useState } from "react";
 function TopRestaurants(){
+
+    const [topRestaurants,setTopRestaurants] = useState(TOP_RESTAURANTS_NEAR_ME);
+
     let searchValue = "";
+    // let topRestaurants = TOP_RESTAURANTS_NEAR_ME;
+    function handleSearch(){
+        setTopRestaurants(TOP_RESTAURANTS_NEAR_ME.filter((restaurant)=>{
+            return restaurant.info.name.toLowerCase().includes(searchValue.toLowerCase());
+        }));
+    }
     return (
         <>
         <div className="search">
             <input className="search-input" placeholder="Search restaurants" onChange={(event)=> {searchValue=event.target.value}} />
-            <button className="search-button" onClick={()=>{console.log(searchValue)}}>Search</button>
+            <button className="search-button" onClick={handleSearch}>Search</button>
         </div>
         <h1>Top Restaurants</h1>
         <div className="grid grid-cols-4 mx-2 my-4 gap-2" >
-            {TOP_RESTAURANTS_NEAR_ME.map((restaurant)=>{
+            {topRestaurants.map((restaurant)=>{
                 return <RestaurantCard key={restaurant.info.id} {...restaurant}/>
             })}
         </div>
